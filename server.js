9c765a0780c88ffc77ec3a1aae43dd84269fb0b2
Bruno Gomes <bruno@evolvest.com.br>
2020-05-17 07:50:44 -0300

    Subindo projeto para o github

---
+++ server.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const server = express()
+
+const db = require("./db")
+
+server.use(express.static('public'))
+server.use(express.urlencoded({ extended: true}))
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("views", {
+    express: server,
+    noCache: true,
+})
+
+server.get("/", function (req, res) {
+
+    db.all(`SELECT * FROM ideas`, function (err, rows) {
+        if (err) {
+            console.log(err)
+            res.send("Erro no banco de dados!")
+        }
+
+        const reverseIdeas = [...rows].reverse()
+
+        let lastIdeas = []
+        for (let idea of reverseIdeas) {
+            if (lastIdeas.length < 3) {
+                lastIdeas.push(idea)
+            }
+        }
+        return res.render("index.html", { ideas: lastIdeas })
+    })
+})
+
+server.get("/ideas", function (req, res) {
+    db.all(`SELECT * FROM ideas`, function (err, rows) {
+        if (err) {
+            console.log(err)
+            res.send("Erro no banco de dados!")
+        }
+
+        const reverseIdeas = [...rows].reverse()
+
+        return res.render("ideias.html", { ideas: reverseIdeas })
+    })
+})
+
+server.post("/createIdea", function(req, res){
+    const data = req.body
+
+    // Inserir dado na tabela
+    const query = `INSERT INTO ideas(
+            image,
+            title,
+            category,
+            description,
+            link
+        ) VALUES (?, ?, ?, ?, ?);`
+
+    const values = [
+        data.image,
+        data.title,
+        data.category,
+        data.description,
+        data.link
+    ]
+
+    db.run(query, values, function(err){
+        if (err) {
+            console.log(err)
+            res.send("Erro no banco de dados!")
+        }
+
+        return res.redirect("/ideas")
+    })
+})
+
+server.get("/deleteIdea/:id", function(req, res){
+    // Deletar dado na tabela
+    db.run(`DELETE FROM ideas WHERE id = ?`, [req.params.id], function (err) {
+        if (err) {
+            console.log(err)
+            res.send("Erro no banco de dados!")
+        }
+
+        return res.redirect("/ideas")
+    })
+})
+
+server.listen(3000)
